f3be942538441c718c83572f79a56927f6fd17b9
mjarraya <mjarraya@student.42.fr>
2019-11-13 11:59:25 +0100

    serialize, deserialize, LocalStrategy

--- passport-auth/app.js
+++ passport-auth/app.js
@@ -48,6 +48,50 @@ app.use(
   })
 );
 
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const User = require("./models/User");
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+const bcrypt = require("bcrypt");
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then(user => {
+        if (!user) {
+          done(null, false, { message: "Invalid credentials" });
+          return;
+        }
+        return bcrypt.compare(password, user.password).then(bool => {
+          if (bool === false) {
+            done(null, false, { message: "Invalid credentials" });
+          } else {
+            // passwords match
+            done(null, user);
+          }
+        });
+      })
+      .catch(err => {
+        done(err);
+      });
+  })
+);
+
 // Express View engine setup
 
 app.use(
